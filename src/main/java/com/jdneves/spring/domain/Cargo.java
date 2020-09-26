f7fba4a695e8cf1f09e6b16d8636cde01403a5cb
Developer-Neves <developerneves@gmail.com>
2020-09-26 00:37:01 -0300

    Incluindo validação campo a campo no fomulário de cadastro de funcionário

--- src/main/java/com/jdneves/spring/domain/Cargo.java
+++ src/main/java/com/jdneves/spring/domain/Cargo.java
@@ -8,15 +8,22 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 
 @SuppressWarnings("serial") // só para esconder warnings
 @Entity                     // Informar para o Spring que essa classe é uma entidade
 @Table(name = "CARGOS")     // Nome da entidade no banco
 public class Cargo extends AbstractEntity<Long> {
 	
+	@NotBlank(message = "O campo cargo é de preenchimento obrigatório.")
+	@Size(max = 60, message = "O nome do cargo deve ter entre no máximo {max} caracteres.")
 	@Column(name = "nome", nullable = false, unique = true, length = 60) // Nome da coluna (atributo) e configuração que será definido no DB
 	private String nome;
 	
+	@NotNull(message = "Selecione um departamento relativo ao cargo.")
 	@ManyToOne
 	@JoinColumn(name = "id_departamento_fk")
 	private Departamento departamento;
